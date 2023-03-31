23f8a9e83ce1989ad13f9393c53afd030cfe6879
Tri Le <tri.le@ninjavan.co>
2023-03-30 22:21:11 -0400

    show preview img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -79,6 +79,7 @@ export default function Home() {
         <div className="border-teal-700">
           <canvas id="drawing-sheet" width={280} height={280} />
         </div>
+        {src && <img src={src} alt="" className="h-[280px] w-[280px]" />}
         {prediction !== undefined && (
           <div className="flex justify-center items-center text-9xl">
             {prediction}
